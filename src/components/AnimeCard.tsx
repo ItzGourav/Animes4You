abfc4570eea67de0be96825e2d531618ffcc4359
Gourav <gourav98055@gmail.com>
2023-09-04 00:57:28 +0530

    minor

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -13,11 +13,11 @@ export default function AnimeCard({ item, isPopular, pos }: { item: animeT, isPo
                 <div className='line-clamp-1'>{item?.name}</div>
             </div>}
             <div className='relative h-[250px] group rounded-md overflow-hidden cursor-pointer'>
-                {item?.isTrending && <div className='bg-[#dd3333] absolute top-1 left-1 z-[3]'><FaFire /></div>}
+                {item?.isTrending && <div className='bg-[#dd3333] p-[6px] absolute top-1 left-1 z-[3] rounded-full text-lg'><FaFire /></div>}
                 <div className='absolute z-[2] invisible group-hover:visible top-0 left-0 h-full w-full bg-surface/70 grid place-content-center'><FaPlay className="text-2xl" /></div>
                 <Image fill className='object-cover group-hover:scale-[1.1] transition-all z-[1]' src={item?.image} alt='image' />
-                <div className='absolute bottom-0 right-0 z-[4]'>
-                    <Chip color='warning' sx={{ padding: "5px 15px", borderRadius: 0, borderTopLeftRadius: "10px" }}>{item?.subOrDub}</Chip>
+                <div className='absolute top-0 right-0 z-[4]'>
+                    <Chip color='warning' sx={{ padding: "5px 15px", borderRadius: 0, borderBottomLeftRadius: "10px" }}>{item?.subOrDub}</Chip>
                 </div>
             </div>
             <div className='flex justify-between mt-2'>
