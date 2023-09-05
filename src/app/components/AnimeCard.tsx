b1d7a912634faad79ddc19d18b220c471b35ea22
Gourav <gourav98055@gmail.com>
2023-09-05 21:36:21 +0530

    fix: card responsoive issue

--- src/app/components/AnimeCard.tsx
+++ src/app/components/AnimeCard.tsx
@@ -12,14 +12,14 @@ export default function AnimeCard({ item, isPopular, pos }: { item: animeT, isPo
                 <div className='text-2xl font-semibold text-primary'>{pos && pos < 10 ? 0 : null}{pos}</div>
                 <div className='line-clamp-1'>{item?.name}</div>
             </div>}
-            <div className='relative group rounded-md overflow-hidden cursor-pointer'>
+            <div className='relative aspect-[0.89] sm:aspect-[0.69] group rounded-md overflow-hidden cursor-pointer'>
                 {item?.isTrending && <div className='bg-[#dd3333] p-[6px] absolute top-1 left-1 z-[3] rounded-full text'><FaFire /></div>}
                 <div className='absolute z-[2] invisible group-hover:visible top-0 left-0 h-full w-full bg-surface/70 grid place-content-center'><FaPlay className="text-2xl" /></div>
-                <Image height={250} width={230} className='max-h-[250px] object-cover group-hover:scale-[1.1] transition-all z-[1]' src={item?.image} alt='image' />
+                <Image fill className='object-cover group-hover:scale-[1.1] transition-all z-[1]' src={item?.image} alt='image' />
                 <div className='absolute top-0 right-0 z-[4] '>
                     <Chip color='warning' radius='none' className='px-1 rounded-bl-xl'>{item?.subOrDub}</Chip>
                 </div>
-                {!isPopular && <div className='text-[13px] bg-black/80 flex justify-center items-center px-2 py-2 absolute bottom-0 left-0 w-full'>
+                {!isPopular && <div className='text-[13px] bg-black/80 flex justify-center items-center px-2 py-3 absolute bottom-0 left-0 w-full z-[2]'>
                     {item?.name}
                 </div>}
             </div>
