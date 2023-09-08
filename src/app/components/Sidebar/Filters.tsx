2d5dc90ebcfccec70493c6228fefee995d0044fc
Gourav <gourav98055@gmail.com>
2023-09-09 01:55:08 +0530

    changes

--- src/app/components/Sidebar/Filters.tsx
+++ src/app/components/Sidebar/Filters.tsx
@@ -7,21 +7,18 @@ import { useRouter } from 'next/navigation';
 
 export default function Filters({ isSearch = false }: { isSearch?: boolean }) {
     const router = useRouter()
-    const [genre, setGenre] = useState<any>(new Set([""]))
-    const [season, setSeason] = useState(new Set([""]))
-    const [studio, setStudio] = useState(new Set([""]))
-    const [status, setStatus] = useState("")
-    const [type, setType] = useState("")
-    const [order, setOrder] = useState("")
+    const [genre, setGenre] = useState<any>(new Set([]))
+    const [season, setSeason] = useState<any>(new Set([]))
+    const [studio, setStudio] = useState<any>(new Set([]))
+    const [status, setStatus] = useState<any>("")
+    const [type, setType] = useState<any>("")
+    const [order, setOrder] = useState<any>("")
 
 
-    useEffect(() => {
-        console.log("Order: ", order)
-        console.log("genre: ", genre)
-    }, [order, genre])
 
     return (
         <div className='bg-white/5 rounded-md'>
+
             <div className='p-2'>
                 <div className={`grid grid-cols-2 md:grid-cols-4 ${isSearch ? "xl:grid-cols-4" : "xl:grid-cols-2"} gap-2`}>
                     <Select selectedKeys={genre} onSelectionChange={setGenre} radius='sm' classNames={{ helperWrapper: "h-auto py-1" }} size='sm' label="Genre" multiple items={GENRE} selectionMode='multiple'>
@@ -31,14 +28,14 @@ export default function Filters({ isSearch = false }: { isSearch?: boolean }) {
                             </SelectItem>
                         ))}
                     </Select>
-                    <Select selectedKeys={season} radius='sm' classNames={{ helperWrapper: "h-auto py-1" }} size='sm' label="Season" multiple items={SEASONS} selectionMode='multiple'>
+                    <Select selectedKeys={season} onSelectionChange={setSeason} radius='sm' classNames={{ helperWrapper: "h-auto py-1" }} size='sm' label="Season" multiple items={SEASONS} selectionMode='multiple'>
                         {SEASONS.map((i) => (
                             <SelectItem key={i.value} value={i.value}>
                                 {i.label}
                             </SelectItem>
                         ))}
                     </Select>
-                    <Select selectedKeys={studio} radius='sm' classNames={{ helperWrapper: "h-auto py-1" }} size='sm' label="Studio" multiple items={STUDIOS} selectionMode='multiple'>
+                    <Select selectedKeys={studio} onSelectionChange={setStudio} radius='sm' classNames={{ helperWrapper: "h-auto py-1" }} size='sm' label="Studio" multiple items={STUDIOS} selectionMode='multiple'>
                         {STUDIOS.map((i) => (
                             <SelectItem key={i.value} value={i.value}>
                                 {i.label}
@@ -46,21 +43,21 @@ export default function Filters({ isSearch = false }: { isSearch?: boolean }) {
                         ))}
                     </Select>
 
-                    <Select selectedKeys={status} value={status} onSelectionChange={(e: any) => setStatus(e)} radius='sm' size='sm' label="Status">
+                    <Select selectedKeys={status} value={status} onSelectionChange={setStatus} radius='sm' size='sm' label="Status">
                         {STATUSES.map((i) => (
                             <SelectItem key={i.value} value={i.value}>
                                 {i.label}
                             </SelectItem>
                         ))}
                     </Select>
-                    <Select selectedKeys={type} value={type} onSelectionChange={(e: any) => setType(e)} radius='sm' size='sm' label="Type">
+                    <Select selectedKeys={type} value={type} onSelectionChange={setType} radius='sm' size='sm' label="Type">
                         {MEDIATYPES.map((i) => (
                             <SelectItem key={i.value} value={i.value}>
                                 {i.label}
                             </SelectItem>
                         ))}
                     </Select>
-                    <Select selectedKeys={order} value={order} onSelectionChange={(e: any) => setOrder(e)} radius='sm' size='sm' label="Order by">
+                    <Select selectedKeys={order} value={order} onSelectionChange={setOrder} radius='sm' size='sm' label="Order by">
                         {SORT.map((i) => (
                             <SelectItem key={i.value} value={i.value}>
                                 {i.label}
