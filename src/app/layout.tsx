202221c74e43d7f8878509476e01cbfc3d0fd52d
Gourav <gourav98055@gmail.com>
2023-09-10 01:11:36 +0530

    feat: footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { Providers } from './Providers'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
                         <Navbar />
                         {children}
                     </div>
+                    <Footer />
                 </Providers>
             </body>
         </html>
